cfa181777f0d6698841d1b36d89ab80e183f022e
Sven Koitka <sven.koitka@uk-essen.de>
2020-05-20 23:27:06 +0200

    ENH: Edit on Github links in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -54,6 +54,14 @@ html_theme_options = {
     'navigation_depth': 3,
 }
 
+html_context = {
+    'display_github': True,
+    'github_user': 'razorx89',
+    'github_repo': 'pydicom-seg',
+    'github_version': 'master',
+    'conf_py_path': '/docs/'
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
