04971d9179499cd3a6e10a81a0d1e2ada6091987
Sven Koitka <sven.koitka@uk-essen.de>
2022-11-24 16:06:56 +0100

    Add import for FractionalWriter

--- pydicom_seg/__init__.py
+++ pydicom_seg/__init__.py
@@ -2,9 +2,10 @@ __version__ = "0.4.0"
 
 from pydicom_seg import template
 from pydicom_seg.reader import MultiClassReader, SegmentReader
-from pydicom_seg.writer import MultiClassWriter
+from pydicom_seg.writer import FractionalWriter, MultiClassWriter
 
 __all__ = [
+    "FractionalWriter",
     "MultiClassReader",
     "MultiClassWriter",
     "SegmentReader",
